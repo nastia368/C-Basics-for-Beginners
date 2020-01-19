6e1ce8f6adbdc255f024910d03510272131dfd57
Anastasiia Stadnik <nastia.st.368@gmail.com>
2020-01-19 21:23:00 +0200

    add two new practice

---
+++ S5_ConditionalStatements/S5_ConditionalStatements/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace S5_ConditionalStatements
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning!");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon!");
+            }
+            else
+            {
+                Console.WriteLine("It's evening!");
+            }
+            
+
+            bool isGoldCustomer = true;
+            /*float price;
+            if (isGoldCustomer)
+                price = 19.95f;
+            else
+                price = 29.95f; */
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            Console.WriteLine(price);
+
+            var season = Season.Spring;
+            switch (season)
+            {
+                case Season.Automn:
+                    Console.WriteLine("It's autumn and a beautiful season.");
+                    break;
+
+                case Season.Summer:
+                    Console.WriteLine("It's perfect to go to the beatch");
+                    break;
+
+                case Season.Spring:
+                case Season.Winter:
+                    Console.WriteLine("We've got a promotion!");
+                    break;
+
+
+                default:
+                    Console.WriteLine("I don't understand this season!");
+                    break;
+
+
+            }
+        }
+    }
+}
---
+++ S5_FirstExercises/S5_FirstExercises/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace S5_FirstExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Exercise
+            int number = 0;
+            Console.WriteLine("Enter a numer between 1 to 10:");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number >= 1 && number <= 10)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("Invalid");
+
+
+            //Second Exercise
+            Console.WriteLine("Enter two numers:");
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            int max = (first > second) ? first : second;
+            Console.WriteLine("Maximum is " + max);
+
+
+            //Third Exercise
+            Console.WriteLine("Enter width:");
+            var width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height:");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            var result = (width > height) ? Orientation.Landscape : Orientation.Portrait;
+            Console.WriteLine("Image orientation is " + result);
+
+
+            //Fouth Exercise
+            Console.WriteLine("Enter speed limit:");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter speed of the car:");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            int points = 0;
+            if (carSpeed < speedLimit)
+            {
+                Console.WriteLine("OK!");
+            }
+            else
+            {
+                points = (carSpeed - speedLimit) / 5;
+                if (points >= 12)
+                    Console.WriteLine("License Suspended!");
+                else
+                    Console.WriteLine("The number of demerit points is " + points);
+            }
+        }
+        public enum Orientation
+        {
+            Landscape,
+            Portrait
+        }
+    }
+}
