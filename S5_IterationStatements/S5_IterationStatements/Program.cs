8642de80c74fda90170d2c84d9743d8700597d56
Anastasiia Stadnik <nastia.st.368@gmail.com>
2020-01-20 19:01:37 +0200

    add two new practices

---
+++ S5_IterationStatements/S5_IterationStatements/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace S5_IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop");
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Foreach loop");
+            var name = "John Smith";
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("While loop");
+            var j = 0;
+            while (j <= 10)
+            {
+                if (j % 2 == 0)
+                    Console.WriteLine(j);
+                j++;
+            }
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+
+                Console.WriteLine("@Echo " + input);
+            }
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo " + input);
+                    continue;
+                }
+                break;
+            }
+
+        }
+    }
+}
---
+++ S5_RandomClass/S5_RandomClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace S5_RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 10));
+            }
+
+            Console.Write("\nAnother code: ");
+            Console.WriteLine((int)'a');
+
+            Console.Write("\nAnother code: ");
+            for (var i = 0; i < 10; i++)
+            {
+                Console.Write((char)random.Next(97, 122));
+            }
+
+            Console.Write("\nAnother code: ");
+            for (var i = 0; i < 10; i++)
+            {
+                Console.Write((char)('a' + random.Next(0, 26)));
+            }
+
+            Console.Write("\nAnother code: ");
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+            Console.Write(password);
+        }
+    }
+}
