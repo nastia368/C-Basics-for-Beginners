9a58f2ba4f8b2a1e90ce0f5c5f89807a1d5342f5
Anastasiia Stadnik <nastia.st.368@gmail.com>
2020-02-27 14:14:57 +0200

    add section 9

--- S9_WorkingWithFiles/S9_WorkingWithFiles/Program.cs
+++ S9_WorkingWithFiles/S9_WorkingWithFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace S9_WorkingWithFiles
 {
@@ -6,7 +7,37 @@ namespace S9_WorkingWithFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            File.Copy(@"c:\temp\test.jpg", @"d:\temp\test.jpg");            var path = @"c:\something.jpg;";
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var constant = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+
+            Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = Directory.GetFiles(@"c:\projects\S9_WorkWithFiles", "*.sln*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\S9_WorkWithFiles", ".", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
         }
     }
 }
